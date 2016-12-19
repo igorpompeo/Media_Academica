4f14a6ddafb5afa33acb64fb993a3784ac3e5012
igorpompeo <pompbass@gmail.com>
2016-12-19 14:32:23 -0200

    Classe Media
    Criação da classe media

---
+++ media_classe.java
@@ -0,0 +1,27 @@
+public class Media{
+	private double n1,trab,n2,mfinal;
+	
+	public double getN1 (){
+		return n1;
+	}
+	public double getN2 (){
+		return n2;
+	}
+	public double getMfinal (){
+		return mfinal = n1+n2;
+	}
+	public double getTrab (){
+		return trab;
+	}
+	
+	public void setN1(double n1){
+		this.n1 = n1;
+	}
+	public void setN2(double n2){
+		this.n2 = n2;
+	}
+	public void setTrab(double trab){
+		this.trab = trab;
+	}
+	
+}
